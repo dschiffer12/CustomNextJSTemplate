72db4c3be049dfd73ccbec3a06db23597915ff3d
dschiffer12 <dschiffer12@gmail.com>
2023-10-08 13:36:43 -0600

    Initial commit of template. Authentication, backen

---
+++ src/app/api/uploadthing/core.ts
@@ -0,0 +1,38 @@
+import { db } from "@/db";
+import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server";
+import { createUploadthing, type FileRouter } from "uploadthing/next";
+ 
+const f = createUploadthing();
+ 
+const auth = (req: Request) => ({ id: "fakeId" }); // Fake auth function
+ 
+
+export const ourFileRouter = {
+
+  fileUploader: f({ "application/pdf": { maxFileSize: "4MB" } })
+
+    .middleware(async ({ req }) => {
+      const { getUser } = getKindeServerSession()
+      const user = getUser();
+ 
+      if (!user || !user.id) throw new Error("Unauthorized");
+
+      return { userId: user.id };
+    })
+    .onUploadComplete(async ({ metadata, file }) => {
+      const createdFile = await db.file.create({
+        data: {
+          key: file.key,
+          name: file.name,
+          userId: metadata.userId,
+          url: `https://uploadthing-prod.s3.us-west-2.amazonaws.com/${file.key}`,
+          uploadStatus: "PROCESSING"
+        }
+      })
+      console.log("Upload complete for userId:", metadata.userId);
+ 
+      console.log("file url", file.url);
+    }),
+} satisfies FileRouter;
+ 
+export type OurFileRouter = typeof ourFileRouter;
---
+++ src/components/ChatWrapper.tsx
@@ -0,0 +1,5 @@
+const ChatWrapper = () => {
+    <div><h1>Chat Wrapper</h1></div>
+}
+
+export default ChatWrapper

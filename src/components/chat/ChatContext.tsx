828116272cb6fb643a6f4557e4a3f7495476b90b
dschiffer12 <dschiffer12@gmail.com>
2023-10-10 21:17:41 -0600

    Chat

---
+++ src/components/chat/ChatContext.tsx
@@ -0,0 +1,59 @@
+import React, { ReactNode, createContext, useState } from "react";
+import { useToast } from "../ui/use-toast";
+import { useMutation } from "@tanstack/react-query";
+
+type StreamResponse = {
+    addMessage: () => void,
+    message: string,
+    handleInputChange: (event: React.ChangeEvent<HTMLTextAreaElement>) => void,
+    isLoading: boolean,
+}
+
+export const ChatContext = createContext({
+    addMessage: () => {},
+    message: "",
+    handleInputChange: () => {},
+    isLoading: false,
+})
+
+interface Props {
+    fileId: string,
+    children: ReactNode
+}
+
+export const ChatContextProvider = ({fileId, children}: Props) => {
+    const [message, setMessage] = useState<string>("")
+
+    const {toast} = useToast()
+
+    const { mutate: sendMessage } = useMutation({
+        mutationFn: async ({message}: {message: string}) => {
+            const response = await fetch("/api/message", {
+                method: 'POST',
+                body: JSON.stringify({
+                    fileId,
+                    message,
+                })
+            })
+            if(response.ok) {
+                throw new Error("Failed to send message")
+            }
+
+            return response.body
+        },
+    })
+
+    const handleInputChange
+
+    const addMessage = () => sendMessage({ message })
+    return (
+        <ChatContext.Provider value={{
+            addMessage,
+            message,
+            handleInputChange,
+            isLoading
+        }}>
+            {children}
+        </ChatContext.Provider>
+    )
+}
--- src/components/chat/ChatInput.tsx
+++ src/components/chat/ChatInput.tsx
@@ -13,7 +13,7 @@ const ChatInput = ({isDisabled}: ChatInputProps) => {
                 <div className="relative flex flex-col w-full flex-grow p-4">
                     <div className="relative">
                         <Textarea rows={1} maxRows={4} autoFocus placeholder="Enter your question..." className='resize-none pr-12 text-base py-3 scrollbar-thumb-blue scrollbar-thumb-rounded scrollbar-track-blue-lighter scrollbar-w-2 scrolling-touch'/>
-                        <Button aria-label="send message">
+                        <Button className="absolute bottom-1.5 right-[8px]" aria-label="send message">
                             <Send className="h-4 w-4"/>
                         </Button>
                     </div>

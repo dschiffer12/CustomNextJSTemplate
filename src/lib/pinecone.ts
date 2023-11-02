56f04704b5368b69c21b95fc24def7debc945a8d
dschiffer12 <dschiffer12@gmail.com>
2023-11-02 11:18:26 -0600

    Commit

--- src/lib/pinecone.ts
+++ src/lib/pinecone.ts
@@ -1,12 +1,3 @@
-import { PineconeClient } from '@pinecone-database/pinecone'
+import { Pinecone } from "@pinecone-database/pinecone";
 
-export const getPineconeClient = async () => {
-  const client = new PineconeClient()
-
-  await client.init({
-    apiKey: process.env.PINECONE_API_KEY!,
-    environment: 'us-central1-gcp-free',
-  })
-
-  return client
-}
+export const pinecone = new Pinecone();
